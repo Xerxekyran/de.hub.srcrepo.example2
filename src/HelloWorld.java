dab50a563cdfb6e05ea5c49b95e4206535dbe1b7
Lars <lars.george@gmx.net>
2013-07-30 13:24:36 +0200

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		(new HelloWorld()).sayHello();
+	}
+	
+	public void sayHello() {
+		System.out.println("Hello World");
+	}
+}
