5b94b3c91793aee286a7425b3366499a1de6f41d
Lars <lars.george@gmx.net>
2013-07-30 13:30:09 +0200

    removing testclass

---
+++ src/de/hub/srcexample2/HelloWorld.java
@@ -0,0 +1,11 @@
+package de.hub.srcexample2;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		(new HelloWorld()).sayHello();
+	}
+	
+	public void sayHello() {
+		System.out.println("Hello World");
+	}
+}
