c40f36591ac40019f3e55f01d233f9ace1215cbb
Lars <lars.george@gmx.net>
2013-07-30 16:25:30 +0200

    added a dependency

--- src/de/hub/srcexample2/HelloWorld.java
+++ src/de/hub/srcexample2/HelloWorld.java
@@ -3,6 +3,8 @@ package de.hub.srcexample2;
 public class HelloWorld {
 	public static void main(String[] args) {
 		(new HelloWorld()).sayHello();
+		
+		NewClass dependency = new NewClass();
 	}
 	
 	public void sayHello() {
