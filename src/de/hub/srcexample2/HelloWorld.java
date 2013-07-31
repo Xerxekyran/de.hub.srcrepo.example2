e48db3905bc5618868208ff681b62ad900799a8e
Lars <lars.george@gmx.net>
2013-07-31 09:37:08 +0200

    added an importable class

--- src/de/hub/srcexample2/HelloWorld.java
+++ src/de/hub/srcexample2/HelloWorld.java
@@ -1,10 +1,15 @@
 package de.hub.srcexample2;
 
+import de.hub.srcexample2.imported.Player;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		(new HelloWorld()).sayHello();
 		
 		NewClass dependency = new NewClass();
+		Player p = new Player();
+		p.setAge(20);
+		System.out.println(p.getAge());
 	}
 	
 	public void sayHello() {
---
+++ src/de/hub/srcexample2/imported/Player.java
@@ -0,0 +1,15 @@
+package de.hub.srcexample2.imported;
+
+public class Player {
+    private int age = 12;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
