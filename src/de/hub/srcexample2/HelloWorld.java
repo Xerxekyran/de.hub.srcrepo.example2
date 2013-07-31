488d265e8e0ae65afd89a6cddd2a4d0a636a080a
Lars <lars.george@gmx.net>
2013-07-31 11:49:14 +0200

    adding method with parameters

--- src/de/hub/srcexample2/HelloWorld.java
+++ src/de/hub/srcexample2/HelloWorld.java
@@ -3,7 +3,12 @@ package de.hub.srcexample2;
 import de.hub.srcexample2.imported.Player;
 
 public class HelloWorld {
-	public static void main(String[] args) {
+	/**
+	 * Starting point of the program
+	 * 
+	 * @param args argument list of the console
+	 */
+    public static void main(String[] args) {
 		(new HelloWorld()).sayHello();
 		
 		NewClass dependency = new NewClass();
@@ -12,7 +17,19 @@ public class HelloWorld {
 		System.out.println(p.getAge());
 	}
 	
+	/**
+	 * 
+	 */
 	public void sayHello() {
 		System.out.println("Hello World");
+	}
+	
+	/**
+	 * 
+	 * @param str
+	 * @param p
+	 */
+	public void say(String str, Player p) {
+	    System.out.println(str);
 	}
 }
