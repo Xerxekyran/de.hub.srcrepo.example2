962499cf2c24729ffd34b8efeff20ebe051be69b
Lars <lars.george@gmx.net>
2013-08-01 11:15:30 +0200

    adding newClass dependency in method call

--- src/de/hub/srcexample2/HelloWorld.java
+++ src/de/hub/srcexample2/HelloWorld.java
@@ -3,35 +3,37 @@ package de.hub.srcexample2;
 import de.hub.srcexample2.imported.Player;
 
 public class HelloWorld {
-	/**
-	 * Starting point of the program
-	 * 
-	 * @param args argument list of the console
-	 */
+    /**
+     * Starting point of the program
+     * 
+     * @param args
+     *            argument list of the console
+     */
     public static void main(String[] args) {
-		(new HelloWorld()).sayHello();
-		
-		NewClass dependency = new NewClass();
-		Player p = new Player();
-		p.setAge(20);
-		System.out.println(p.getAge());
-	}
-	
-	/**
+	NewClass dependency = new NewClass();
+	(new HelloWorld()).sayHello(dependency);
+
+	Player p = new Player();
+	p.setAge(20);
+	System.out.println(p.getAge());
+    }
+
+    /**
 	 * 
 	 */
-	public void sayHello() {
-		System.out.println("Hello World");
-	}
-	
-	/**
-	 * 
-	 * @param str
-	 * @param p
-	 */
-	public void say(String str, Player p) {
-	    System.out.println(str);
-	}
-	
-	public void say2() {}
+    public void sayHello(NewClass nc) {
+	System.out.println("Hello World");
+    }
+
+    /**
+     * 
+     * @param str
+     * @param p
+     */
+    public void say(String str, Player p) {
+	System.out.println(str);
+    }
+
+    public void say2() {
+    }
 }
